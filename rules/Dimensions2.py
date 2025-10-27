81fdd4fa377580952af314b3656d76b47cd93aa4
JoeS223919 <s223919@student.dtu.dk>
2025-10-27 14:24:11 +0100

    1

--- rules/Dimensions2.py
+++ rules/Dimensions2.py
@@ -44,8 +44,8 @@ def beam_dimensions(model):
         global_id = getattr(beam, "GlobalId", None)
         if global_id:
             results[global_id] = {
-                "h": dimensions.get("height"),
-                "b": dimensions.get("width"),
+                "h": dimensions.get("h"),
+                "b": dimensions.get("b"),
                 "l": cut_length
             }
 
